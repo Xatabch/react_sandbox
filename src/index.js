f7272d8e426ea39656a75ab2a235ae05570c0238
Ivan <ivan.morozov@list.ru>
2020-05-24 03:51:22 +0300

    add sentry

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import * as Sentry from '@sentry/browser';
 import App from './Containers/App';
-
 import 'normalize.css';
+
+Sentry.init({dsn: "https://5556d1476bd048978cf71508fa0208c8@o397203.ingest.sentry.io/5251478"});
 
 ReactDOM.render(
     <App/>,
