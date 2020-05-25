eb5eab6e19082dc87b7fe0f869fa8c76889dac69
Ivan <ivan.morozov@list.ru>
2020-05-26 02:32:22 +0300

    react-nodejs: add FCP

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom';
 import * as Sentry from '@sentry/browser';
 import App from './Containers/App';
 import 'normalize.css';
+import {getFCP} from 'web-vitals';
 
 Sentry.init({dsn: "https://5556d1476bd048978cf71508fa0208c8@o397203.ingest.sentry.io/5251478"});
+
+// Measure and log the current FCP value,
+// any time it's ready to be reported.
+getFCP(console.log);
 
 ReactDOM.render(
     <App/>,
