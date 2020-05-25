9c6970d0cd863fc86000033991d7bd1b50cdf421
Ivan <ivan.morozov@list.ru>
2020-05-25 15:07:11 +0300

    react-nodejs: add dummy

--- prometheus/index.js
+++ prometheus/index.js
@@ -4,8 +4,10 @@ var cors = require('cors');
 
 app.use(cors());
 
+
 // Инит метрик
 const registry = new client.Registry();
+
 const gauge = new client.Gauge({
     name: 'active_users',
     help: 'Amount of active users right now per category',
@@ -14,6 +16,29 @@ const gauge = new client.Gauge({
       'category',
     ],
 });
+
+activeUsersPerCategoryMetric(registry);
+
+function activeUsersPerCategoryMetric(registry) {    
+    // To make data looks more
+    const categoriesWithDistribution = [
+      ['oil', 100, 30],
+      ['wine', 200, 30],
+      ['bread', 300, 30],
+      ['butter', 400, 30],
+    ];
+    
+    async function collectActiveUsers() {
+      categoriesWithDistribution.map(async ([category, mean, variance]) => {
+        gauge.set(
+          { category },
+          Math.floor(gaussian(mean, variance).ppf(Math.random())),
+        );
+      });
+    }
+    
+    setInterval(collectActiveUsers, 5000);
+}
 
 app.get('/metrics', (req, res, next) => {
     res.set('Content-Type', registry.contentType);
